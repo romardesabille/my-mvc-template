79d4c769f400fe3b9afd955a049c75c4981fbfb5
Romar <59155023+romardesabille@users.noreply.github.com>
2020-08-25 21:23:20 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    $config['base_url'] = 'http://localhost:8080/MVC-Template/';
+    $config['base_url'] = 'https://my-mvc-test.herokuapp.com/';
 
     //set empty when not using a root folder in url
-    $config['root_folder'] = 'MVC-Template';
+    $config['root_folder'] = '';
 
     $config['charset'] = 'UTF-8';
 
